2d183f9a560ad4e0b602d2a14e5c689b9f7cdeb3
Oleksiy <lenor5520@gmail.com>
2026-07-19 22:45:18 +0300

    Update to-do_list.py

--- to-do_list.py
+++ to-do_list.py
@@ -1,3 +1,4 @@
+#Display the main menu
 def show_menu():
    print("=" * 20)
    print("     TO-DO LIST")
@@ -7,15 +8,17 @@ def show_menu():
    print("2. Add task")
    print("3. Delete task")
    print("4. Exit")
+#Get user's menu choice
 def get_choice():
    choice = input("Choose option: ")
 
    return choice
+#Add a new task
 def add_task():
    task = input("Enter your task: ")
 
    tasks.append(task)
-
+#Display all tasks
 def show_tasks():
    if len(tasks) == 0:
       print("No tasks yet!")
@@ -23,14 +26,22 @@ def show_tasks():
       for index,task in enumerate(tasks, start=1):
          print(index, task)
 
+# Delete selected task
 def delete_task():
-   if len(tasks) == 0:
-      print("No tasks yet!")
-   else:
-      show_tasks()
-      number = int(input("Which task do you want to delete?: "))
-      tasks.pop(number - 1)
-      print("Task deleted!")
+    if len(tasks) == 0:
+        print("No tasks yet!")
+    else:
+        try:
+         show_tasks()
+         number = int(input("Which task do you want to delete?: "))
+         if 1 <= number <= len(tasks):
+            tasks.pop(number - 1)
+            print("Task deleted!")
+         else:
+            print("Please enter a valid number!")
+        except ValueError:
+            print("Please enter a valid number!")
+
       
 
 
