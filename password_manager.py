22af9886581397c01ba437b13715b93ec5b94509
Oleksiy <lenor5520@gmail.com>
2026-07-21 21:29:54 +0300

    Update password_manager.py

--- password_manager.py
+++ password_manager.py
@@ -1,3 +1,4 @@
+#Display the main menu
 def show_menu():
     print("=" * 20)
     print("   PASSWORD MANAGER")
@@ -9,12 +10,14 @@ def show_menu():
     print("4. Delete account")
     print("5. Exit")
 
+#Get user's menu choice
 def get_choice():
     choice = input("Your choice: ")
 
     if choice in ["1", "2", "3", "4", "5"]:
         return choice
     
+#Add a new account
 def add_account():
     service = input("Enter your service: ")
     login = input("Enter your login: ")
@@ -23,9 +26,58 @@ def add_account():
     with open("accounts.txt", "a") as file:
         file.write(f"{service} | {login} | {password}\n")
 
+#Display all saved accounts
+def show_accounts():
+    with open("accounts.txt", "r") as file:
+        content = file.read()
+
+        
+
+        if not content:
+            print("No accounts yet!")
+        else:
+            print(content)
+        
+
+#Search for an account by service name
+def find_account():
+    service = input("Enter service to find: ")
+
+    with open("accounts.txt", "r") as file:
+        found = False
+        for line in file:
+            if service in line:
+                print(line)
+                found = True
+        
+        if not found:
+            print("Account not found!")
+#Delete an account by service name
+def delete_account():
+    service = input("Select the account you want to delete: ")
+    new_lines = []
+    with open("accounts.txt", "r") as file:
+        account = file.readlines()
+        
+
+        found = False
+        for line in account:
+            if service in line:
+                found = True
+            else:
+                new_lines.append(line)
+
+        if found:
+            print("Account deleted!")
+
+            with open("accounts.txt", "w") as file:
+                file.writelines(new_lines)
+        else:
+            print("Account not found!")
+
            
 
-
+#Main program loop
 while True:
     show_menu()
 
@@ -33,15 +85,16 @@ while True:
 
 
     if choice == "1":
-        print("1")
+        show_accounts()
     elif choice == "2":
         add_account()
     elif choice == "3":
-        print("3")
+        find_account()
     elif choice == "4":
-        print("4")
+        delete_account()
     elif choice == "5":
-        print("5")
+        print("Goodbye!")
+        break
     else:
         print("Invalid choice! Please enter again.")
         continue
